b31962a6f14e001551b91e6751badd6c848d1373
Osama Ayub <ayubosama416@gmail.com>
2024-11-26 20:10:13 +0500

    portfolio Website updated

---
+++ src/assets/index.js
@@ -0,0 +1,63 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import nextjs from "./tech/nextjs.png"
+import Narsun from "./company/narsun.jpeg"
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import threejs from "./tech/threejs.svg";
+
+
+
+import iifatech from "./company/iifatech.jpeg";
+import Portfolio from "./Portfolio.png";
+import ModernPortfolio from "./3dportfolio.png";
+import JobLane from "./tech/jobLane.png";
+import Express from "./tech/express.png";
+
+export {
+  iifatech,
+  Narsun,
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  JobLane,
+  github,
+  menu,
+  close,
+  css,
+  nextjs,
+  docker,
+  figma,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  tailwind,
+  typescript,
+  threejs,
+  Express,
+  Portfolio,
+  ModernPortfolio
+};
